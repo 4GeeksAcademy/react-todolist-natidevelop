e2725d4d5f6940420453cc826b048127e2fa7a7f
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-10-16 19:23:27 +0000

    agregar y listar tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState([])
+	function agregarTareas(e) {
+		e.preventDefault()
+		if(tarea != ""){
+
+			setLista([...lista, tarea])
+			setTarea("")
+		}else{
+			alert("Ingresar Tarea")
+		}
+		if(e.key==="Enter"){
+			setLista([...lista, tarea])
+			setTarea("")
+		}
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center">
+			<h1 className="text-center mt-5">Todolist</h1>
+			<form className="row">
+				<div className="col-12">
+					<input type="text"
+					//  onKeyDown={(e) => agregarTareas(e)} 
+					 className="form-control" placeholder="task" value={tarea} onChange={(e) => setTarea(e.target.value)} />
+				</div>
+				<div className="col-12 mt-2">
+					<button type="submit" onClick={(e) => agregarTareas(e)} className="btn btn-primary mb-3">Agregar Tarea</button>
+				</div>
+			</form>
+			<ul className="list-group">
+				{lista.map((item, index) => (
+
+					<li className="list-group-item"key={index}>{item}</li>
+				))}
+			</ul>
 		</div>
 	);
 };
