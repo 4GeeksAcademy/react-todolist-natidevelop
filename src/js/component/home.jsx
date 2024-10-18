0fa9fff8efddab82588c0def2bb5f9242b41a12f
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-10-18 23:11:29 +0000

    mi todolist agrega y elimina tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,39 +7,50 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [tarea, setTarea] = useState("")
 	const [lista, setLista] = useState([])
-	function agregarTareas(e) {
-		e.preventDefault()
-		if(tarea != ""){
+	function agregarTareas(evento) {
+		evento.preventDefault()
+		if (tarea != "") {
 
 			setLista([...lista, tarea])
 			setTarea("")
-		}else{
+		} else {
 			alert("Ingresar Tarea")
 		}
-		if(e.key==="Enter"){
+		if (evento.key === "Enter") {
 			setLista([...lista, tarea])
 			setTarea("")
 		}
 	}
 	return (
-		<div className="container text-center">
-			<h1 className="text-center mt-5">Todolist</h1>
+		<div className="container text-center ">
+			<h1 className="text-center mt-5 ">Todos</h1>
 			<form className="row">
-				<div className="col-12">
-					<input type="text"
-					//  onKeyDown={(e) => agregarTareas(e)} 
-					 className="form-control" placeholder="task" value={tarea} onChange={(e) => setTarea(e.target.value)} />
+				<div className="col-12 ">
+					<input type="text "
+						//  onKeyDown={(e) => agregarTareas(e)} 
+						className="form-control border border-primary" placeholder="Nueva Tarea" value={tarea} onChange={(evento) => setTarea(evento.target.value)} />
 				</div>
 				<div className="col-12 mt-2">
-					<button type="submit" onClick={(e) => agregarTareas(e)} className="btn btn-primary mb-3">Agregar Tarea</button>
+					<button type="submit" onClick={(evento) => agregarTareas(evento)} className="btn btn-primary mb-3">Agregar Tarea</button>
 				</div>
 			</form>
-			<ul className="list-group">
+			<ul className="list-group border-primary">
 				{lista.map((item, index) => (
+					<li className="list-group-item border border-primary" key={index}>{item}<i onClick={() => {
 
-					<li className="list-group-item"key={index}>{item}</li>
+						let listaFiltrada = lista.filter((task, subindex) => {
+							return (subindex != index)
+						})
+						setLista(listaFiltrada)
+
+					}}
+						className="m-1 fa-solid fa-trash icono-oculto"></i></li>
 				))}
 			</ul>
+			<span className="text-primary
+			">{(lista.length==0)?"No hay Tareas , Agregar Una ":""}</span>
+			<p></p>
+			<span className="text-success">{lista.length} Items Left</span>
 		</div>
 	);
 };
