664c98ebdccdf43a82cd382925b4b0e68a1b2828
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-10-22 17:54:42 +0000

    tareas con fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,20 +8,112 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [tarea, setTarea] = useState("")
 	const [lista, setLista] = useState([])
-	function agregarTareas(evento) {
+
+	async function agregarTareas(evento) {
 		evento.preventDefault()
 		if (tarea != "") {
-
-			setLista([...lista, tarea])
+			guardarTareas()
 			setTarea("")
 		} else {
 			alert("Ingresar Tarea")
 		}
 		if (evento.key === "Enter") {
-			setLista([...lista, tarea])
+			guardarTareas()
 			setTarea("")
+
 		}
 	}
+
+	const guardarTareas = async () => {
+		try {
+			const url = "https://playground.4geeks.com/todo/todos/nati"
+			const resp = await fetch(url, {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					label: tarea,
+					is_done: false
+
+				})
+			})
+			if (resp.ok) {
+				cargarTareas()
+				return true
+			}
+		} catch (error) {
+			console.log(error)
+			return false
+
+		}
+	}
+	const cargarTareas = async () => {
+		try {
+
+
+			const url = "https://playground.4geeks.com/todo/users/nati"
+			const resp = await fetch(url)
+			if (resp.status == 404) {
+				crearUsuario()
+				return
+			}
+			const data = await resp.json()
+			setLista(data.todos)
+
+			return true
+
+		} catch (error) {
+			console.error(error)
+			return false
+		}
+	}
+
+	const crearUsuario = async () => {
+		try {
+			const resp = await fetch("https://playground.4geeks.com/todo/users/nati", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" }
+
+			})
+			if (resp.status == 201) {
+				cargarTareas()
+			}
+		} catch (error) {
+			console.log(error)
+			return false
+		}
+	}
+
+	const borrar = async (id) => {
+		console.log(id)
+		try {
+			const resp = await fetch("https://playground.4geeks.com/todo/todos/" + id, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" }
+
+
+
+			})
+			console.log(resp.status)
+			if (resp.status == 204) {
+				cargarTareas()
+				return true
+
+			}
+
+		} catch (error) {
+			console.log(error)
+			return false
+
+		}
+	}
+
+	useEffect(() => {
+
+		cargarTareas()
+	}, [])
+
 	return (
 		<div className="container text-center ">
 			<h1 className="text-center mt-5 ">Todos</h1>
@@ -36,19 +129,15 @@ const Home = () => {
 			</form>
 			<ul className="list-group border-primary">
 				{lista.map((item, index) => (
-					<li className="list-group-item border border-primary" key={index}>{item}<i onClick={() => {
-
-						let listaFiltrada = lista.filter((task, subindex) => {
-							return (subindex != index)
-						})
-						setLista(listaFiltrada)
-
-					}}
-						className="m-1 fa-solid fa-trash icono-oculto"></i></li>
+					<li className="list-group-item border border-primary" key={index}>
+						{item.label}
+						<i onClick={() => borrar(item.id)}
+							className="m-1 fa-solid fa-trash icono-oculto"></i>
+					</li>
 				))}
 			</ul>
 			<span className="text-primary
-			">{(lista.length==0)?"No hay Tareas , Agregar Una ":""}</span>
+			">{(lista.length == 0) ? "No hay Tareas , Agregar Una " : ""}</span>
 			<p></p>
 			<span className="text-success">{lista.length} Items Left</span>
 		</div>
